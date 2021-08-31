1023a802acd4e5a4df681637c6273f9d5ba8f899
Yuri Lendro <y.lendrof@gmail.com>
2021-08-31 16:42:24 +0200

    Made simple UI and reactive variables. Organize code in functions

--- src/App.js
+++ src/App.js
@@ -1,82 +1,96 @@
 import "./App.css";
+import { useState } from "react";
 import { tramosIRPF2021 } from "./assets/tramosIRPF.js";
 
-let salarioBrutoAnual = 34000;
-let cuotasSeguridadSocialPagadas = 900;
-let salarioNetoAnual = salarioBrutoAnual - cuotasSeguridadSocialPagadas;
-let minoracionPorGastosSinJustificar = 2000;
-let baseImponibleGeneral = salarioNetoAnual - minoracionPorGastosSinJustificar;
-
-let restante = baseImponibleGeneral;
-let procesado = 0;
-let llegadoUltimoTramo = false;
-
-tramosIRPF2021.forEach((tramo) => {
-  console.log("Procesado:" + procesado);
-  console.log("Restante:" + restante);
-
-  if (!llegadoUltimoTramo) {
-    // cantidad de la que descontar en este tramo
-    let baseTramo;
-    // cantidad porcentuada de la base a descontar (esto es lo que te quitan dentro de este tramo)
-    let sujetoTramo;
-
-    // Averiguar si la baseImponible es mayor o menor que la cantidad maxima de este tramo. Es decir, llenamos el tramo entero o nos quedamos a medias.
-    if (tramo.to <= baseImponibleGeneral) {
-      // Este tramo está por debajo que la base general. Aun quedará por procesar en el siguiente tramo.
-      console.log("el tramo es menor que la base imponible");
-      baseTramo = tramo.to;
-      sujetoTramo = (baseTramo * tramo.porcentaje) / 100;
-    } else {
-      // El tramo es más alto que la base general. Por lo tanto no se habra más procesamientos en el siguiente tramo
-      console.log("el tramo es más alto que la base imponible");
-      baseTramo = tramo.to - baseImponibleGeneral;
-      sujetoTramo = (baseTramo * tramo.porcentaje) / 100;
-      llegadoUltimoTramo = true;
-    }
-
-    restante -= baseImponibleGeneral - baseTramo;
-    procesado += baseTramo;
-    console.log("BaseTramo: " + baseTramo);
-    console.log("sujetoTramo: " + sujetoTramo);
-  } else {
-    console.log("SKIPPPP");
-  }
-  console.log("--------");
-});
-
-tramosIRPF2021.forEach((tramo) =)
-
-const procesarTramoIRPF = (tramo, baseImponible) => {
-  let llegadoLimiteTramos;
-  let baseImponibleTramo = 0;
-  if (tramo.to > baseImponible) {
-    baseImponibleTramo = baseImponible;
-    llegadoLimiteTramos = true;
-  } else if (tramo.to < baseImponible) {
-    baseImponibleTramo = tramo.to;
-  } else if (tramo.to === baseImponible) {
-    llegadoLimiteTramos = true;
-  }
-
-  let baseSujetaTramo = baseImponibleTramo - tramo.from;
-  let aPagar = (baseSujetaTramo * tramo.porcentaje) / 100;
-
-  return {
-    aPagar: aPagar,
-    baseImponibleTramo: baseImponibleTramo,
-    llegadoLimiteTramos: llegadoLimiteTramos,
-  };
-};
-
 function App() {
-  return (
-    <div className="container">
-      <h1>Dashboard</h1>
-      <section></section>
-      <div><pre>{JSON.stringify(tramosIRPF2021, null, 2) }</pre></div>;
-    </div>
-  );
+    const [salarioBrutoAnual, setSalarioBrutoAnual] = useState(0);
+    const [cuotasSeguridadSocialPagadas, setCuotasSeguridadSocialPagadas] =
+        useState(900);
+    const [pagoIRPF, setPagoIRPF] = useState(0);
+
+    let salarioNetoAnual = salarioBrutoAnual - cuotasSeguridadSocialPagadas;
+    let minoracionPorGastosSinJustificar = 2000;
+    let baseImponibleGeneral =
+        salarioNetoAnual - minoracionPorGastosSinJustificar;
+
+    // controlling variables
+    let restanteProcesar = baseImponibleGeneral;
+    let procesado = 0;
+    let llegadoUltimoTramo = false;
+
+    const procesarTramoIRPF = (tramo, baseImponible) => {
+        let llegadoLimiteTramos = false;
+        let baseImponibleTramo = 0;
+        if (tramo.to > baseImponible) {
+            baseImponibleTramo = baseImponible;
+            llegadoLimiteTramos = true;
+        } else if (tramo.to < baseImponible) {
+            baseImponibleTramo = tramo.to;
+        } else if (tramo.to === baseImponible) {
+            llegadoLimiteTramos = true;
+        }
+
+        // Cantidad procesada en este tramos
+        let baseSujetaTramo = baseImponibleTramo - tramo.from;
+        // Cantidad porcentuada de este tramo
+        let aPagar = (baseSujetaTramo * tramo.porcentaje) / 100;
+
+        return {
+            aPagar: aPagar,
+            baseSujetaTramo: baseSujetaTramo,
+            llegadoLimiteTramos: llegadoLimiteTramos,
+            porcentajeTramo: tramo.porcentaje,
+        };
+    };
+
+    const calcularIRPFAnual = () => {
+        let pagoIRPFtmp = 0;
+        tramosIRPF2021.every((tramo) => {
+            let {
+                aPagar,
+                baseSujetaTramo,
+                llegadoLimiteTramos,
+                porcentajeTramo,
+            } = procesarTramoIRPF(tramo, baseImponibleGeneral);
+
+            // cantidad restante por someter a retención
+            restanteProcesar -= baseSujetaTramo;
+            // cantidad que ya ha sido sometida a retención
+            procesado += baseSujetaTramo;
+            // acumular cantidad de IRFP a pagar correspondiente a este tramo al total de IRPF
+            pagoIRPFtmp += aPagar;
+
+            // console.log("A PAGAR:" + aPagar);
+            // console.log("baseSujetaTramos: " + baseSujetaTramo);
+            // console.log("porcentaje: " + porcentajeTramo);
+            // console.log("procesado: " + procesado);
+            // console.log("restanteProcesar: " + restanteProcesar);
+            // console.log("Llegado ultimo Tramos: " + llegadoLimiteTramos);
+            // console.log("---------");
+
+            return !llegadoLimiteTramos;
+        });
+
+        // set IRPF ANUal
+        setPagoIRPF(pagoIRPFtmp);
+        console.log("IRPF a pagar: " + pagoIRPFtmp);
+    };
+
+    const inputHandler = (event) => {
+        setSalarioBrutoAnual(event.target.value);
+    };
+
+    return (
+        <div className="container">
+            <h1>Dashboard</h1>
+            <section></section>
+            <section>
+                <input type="text" onChange={inputHandler} />
+                <button onClick={calcularIRPFAnual}>calcular</button>
+                IRPF TOTAL: {pagoIRPF}
+            </section>
+        </div>
+    );
 }
 
 export default App;
